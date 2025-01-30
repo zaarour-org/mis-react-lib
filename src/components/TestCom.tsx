895506b0ded09522ba51c07dad961bdaceece3d1
Ali Zaarour <zaarour.a@outlook.com>
2025-01-30 02:10:53 +0200

    init

--- src/components/TestCom.tsx
+++ src/components/TestCom.tsx
@@ -1,5 +1,7 @@
-const TestCom = () => {
-    return (<>Hello from TestCom)</>);
+import React from "react";
+
+const TestCom: React.FC = () => {
+    return <h1>Hello from TestCom</h1>;
 };
 
 export default TestCom;
